4ffb93b27d15a7cd3af18c44511dd7ca1026f87c
HellerBarde <heller.barde@gmail.com>
2009-07-29 15:25:30 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Countdown</title>
+<meta name="author" content="Philip Stark" />
+<link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+</head>
+<body>
+<div class="countdown">
+<?php
+include 'settings.php';
+include 'countdown.php';
+?>
+</body>
+</html>
